3fe9daf84355b950e8149d83641e636cc29c0d27
faroob <fkleinrock@gmail.com>
2024-11-18 20:51:15 -0500

    :)

--- src/main/java/net/faroob/movementtest/networking/packet/DashC2SPacket.java
+++ src/main/java/net/faroob/movementtest/networking/packet/DashC2SPacket.java
@@ -12,9 +12,6 @@ public class DashC2SPacket {
     public static void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
         double pitch = player.getPitch();
         double yaw = player.getHeadYaw();
-        double yaw2 = Math.abs(yaw) / yaw;
-        System.out.println(yaw2);
-        //System.out.println(Math.sin(yaw));
         player.velocityModified = true;
         player.addVelocity(-Math.sin(Math.toRadians(yaw)), -Math.sin(Math.toRadians(pitch)), Math.cos(Math.toRadians(yaw)));
     }
